28bbf0ad052137dc2b2a60dee038e4f2fce95bd3
Idrees Khan <dotnetdreamer@users.noreply.github.com>
2025-08-17 19:31:48 +0200

    Fix import statements for SignalRClient and SwiftSignalRClient compatibility

--- ios/Sources/CapacitorSignalRPlugin/CapacitorSignalR.swift
+++ ios/Sources/CapacitorSignalRPlugin/CapacitorSignalR.swift
@@ -1,5 +1,9 @@
 import Foundation
+#if canImport(SignalRClient)
+import SignalRClient
+#elseif canImport(SwiftSignalRClient)
 import SwiftSignalRClient
+#endif
 import SwiftyJSON
 
 @objc public class CapacitorSignalR: NSObject, HubConnectionDelegate {
